cecfe021835485a855d6b2c1a570cb59e74e2156
PauloFerreira-S <70450312+PauloFerreira-S@users.noreply.github.com>
2020-09-14 16:17:24 -0300

    Start Commit

--- app/src/main/java/com/paulo/pagamentodeconta/MainActivity.kt
+++ app/src/main/java/com/paulo/pagamentodeconta/MainActivity.kt
@@ -2,12 +2,14 @@ package com.paulo.pagamentodeconta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+            Seekbar()
         btn_calcular.setOnClickListener(){
           calcular()
         }
@@ -16,12 +18,40 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+    private fun Seekbar(){
+        seekBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener{
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    txt_porcento.text = progress.toString()
+
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+                }
+            }
+
+        )
+
+
+
+    }
     private fun calcular(){
         var Num1 = Num1.text.toString().toInt()
         var Num2 = Num2.text.toString().toInt()
-        Num1 = Num1 + 15
+        var Num3 = txt_porcento.text.toString().toInt()
+        Num1 = Num1 + Num3
         var troco = (Num2 - Num1)
 
+        txt_taxa.text = "R$"+ Num3
         txt_total.text = "R$"+ Num1
         txt_troco.text = "R$"+ troco
     }
